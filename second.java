e8d1b77b66908313eb36f683d74de1a639b07bdc
Galya <mrs.galyna98@gmail.com>
2023-08-27 00:49:37 +0300

    new commit 2

---
+++ second.java
@@ -0,0 +1,17 @@
+package secondcode;
+/*demonstration of variable usage
+ */
+public class secondcode {
+public static void main(String[] args) {
+	int myVar1;
+	int myVar2;
+	myVar1 = 1024;
+	
+	System.out.println("myVar1 is " + myVar1);
+	
+	myVar2 = myVar1 / 2;
+	
+	System.out.print("myVar2 is myVar1 / 2: ");
+	System.out.println(myVar2);
+}
+}
