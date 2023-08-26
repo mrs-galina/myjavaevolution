91f79d049049d092fcfbd6d918fc839cfc22c91c
Galya <mrs.galyna98@gmail.com>
2023-08-26 22:26:08 +0300

    My first code

---
+++ first.java
@@ -0,0 +1,10 @@
+package myfirstcode;
+/* this is me trying to learn how to comment
+ I love writing comments
+ */
+public class myfirstcode {
+	// this is a short comment
+	public static void main(String[] args) {
+		System.out.println("Java rules the world.");
+	}
+}
