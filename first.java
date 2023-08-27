1b07ed13d59e89179c1f69480cfafc1e421053ec
Galya <143288791+mrs-galina@users.noreply.github.com>
2023-08-27 15:30:10 +0300

    first.java

--- first.java
+++ first.java
@@ -6,10 +6,5 @@ public class myfirstcode {
 	// this is a short comment
 	public static void main(String[] args) {
 		System.out.println("Java rules the world.");
-a,msdf
-123sd;fk
-1234
-;dg;]\dfggd
-1111111111111111
 	}
 }
